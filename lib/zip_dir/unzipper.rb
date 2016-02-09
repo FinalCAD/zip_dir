288f339100b9ece0c64f70a0b17a1d7a85dab637
Joel AZEMAR <joel.azemar@gmail.com>
2016-02-09 12:46:41 +0100

    Fix bad zip encoding to create dest folders

--- lib/zip_dir/unzipper.rb
+++ lib/zip_dir/unzipper.rb
@@ -23,6 +23,13 @@ module ZipDir
       ::Zip::File.open(zip_path) do |zip_file|
         zip_file.each do |entry|
           file_path = "#{@unzip_path}/#{entry.name}"
+
+          # Fixes:
+          # Errno::ENOENT:
+          # No such file or directory @ rb_sysopen - /var/folders/6c/s4snqy051jqdpbjw7f7tsn940000gn/T/d20160209-56123-1o14n5n/Niveaux/Fondations.png
+          dir_path = File.dirname(file_path).to_s
+          FileUtils.mkdir_p dir_path unless File.exists?(dir_path)
+
           entry.extract(file_path) unless File.exists?(file_path)
         end
       end
--- lib/zip_dir/version.rb
+++ lib/zip_dir/version.rb
@@ -1,3 +1,3 @@
 module ZipDir
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
