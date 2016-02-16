62d5212327b22cf5e65d1971929e5298d1a6c7a9
Joel AZEMAR <joel.azemar@gmail.com>
2016-02-16 15:50:03 +0100

    enhance ensure on extract path

--- lib/zip_dir/unzipper.rb
+++ lib/zip_dir/unzipper.rb
@@ -22,16 +22,36 @@ module ZipDir
     def unzip
       ::Zip::File.open(zip_path) do |zip_file|
         zip_file.each do |entry|
+          next if unauthorized_file?(entry)
+
           file_path = "#{@unzip_path}/#{entry.name}"
 
           # Fixes: spec/fixtures/encoded_differently.zip (in a test)
-          dir_path = File.dirname(file_path).to_s
-          FileUtils.mkdir_p dir_path unless File.exists?(dir_path)
+          ensure_path_exists file_path
 
           entry.extract(file_path) unless File.exists?(file_path)
         end
       end
       @unzipped = true
+    end
+
+    private
+
+    def unauthorized_file?(file)
+      file.name.match(/MACOSX|.DS_Store/)
+    end
+
+    def ensure_path_exists file_path
+      # Create directory structure if doesn't exists
+      if file_path.to_s.match(/(?<last_character>\/$)/)
+        FileUtils.mkdir_p file_path.to_s unless File.exists?(file_path.to_s)
+      end
+
+      # We are not sure about is a directory structure or not,
+      # we look one level up and we take a look if necessary to create structure or not.
+      unless File.exists?(dirpath = File.dirname(file_path.to_s))
+        FileUtils.mkdir_p dirpath.to_s
+      end
     end
   end
 end
